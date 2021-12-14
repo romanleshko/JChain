be3067fae80bdcc9a67d6814b591c7cf5ec61b4b
Roman Leshko <14238237+romanleshko@users.noreply.github.com>
2021-12-14 18:07:38 +0100

    Add Block class

---
+++ src/main/java/pro/leshko/blockchain/Block.java
@@ -0,0 +1,66 @@
+package pro.leshko.blockchain;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Block {
+
+    private Instant timestamp;
+    private String data;
+    private String prevHash;
+
+    Block(final Instant timestamp, final String data) {
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+
+    Block(final Instant timestamp, final String data, final String prevHash) {
+        this(timestamp, data);
+        this.prevHash = prevHash;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getPrevHash() {
+        return prevHash;
+    }
+
+    public void setPrevHash(String prevHash) {
+        this.prevHash = prevHash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Block block = (Block) o;
+        return Objects.equals(timestamp, block.timestamp) &&
+                Objects.equals(data, block.data) &&
+                Objects.equals(prevHash, block.prevHash);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timestamp, data, prevHash);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Block {timestamp='%s', data='%s', prevHash='%s'}", this.timestamp, this.data, this.prevHash);
+    }
+}
